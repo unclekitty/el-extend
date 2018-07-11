d5273fb20624d126fbb7e0ba594e6160b55eae6e
吴君 <jun.wu1@baozun.com>
2018-07-11 09:11:11 +0800

    1.0.2

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import DatePicker from '@components/DatePicker'
 
-export default {
-  install (Vue) {
-    Vue.components('DatePicker', DatePicker)
-  }
+const install = function (Vue) {
+  Vue.components(DatePicker.name, DatePicker)
 }
+
+module.exports = {
+  version: '1.0.2',
+  install
+}
+
+module.exports.default = module.exports;
