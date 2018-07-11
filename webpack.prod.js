c6896a5ef6c95544773029362bc299ce588282aa
吴君 <jun.wu1@baozun.com>
2018-07-11 16:55:39 +0800

    1.0.18

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,62 +1,47 @@
-const path = require('path');
-const webpack = require('webpack');
-const nodeExternals = require('webpack-node-externals');
+const path = require("path");
+const webpack = require("webpack");
+// const nodeExternals = require("webpack-node-externals");
 
-function resolve (dir) {
-  return path.join(__dirname, '.', dir)
+function resolve(dir) {
+  return path.join(__dirname, ".", dir);
 }
 
 module.exports = {
-  entry: './src/index.js',
-  externals: [nodeExternals()],
+  entry: "./src/index.js",
+  // externals: [nodeExternals()],
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'el-extend.js',
-    libraryTarget: 'commonjs2',
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "/dist/",
+    filename: "el-extend.js",
+    libraryTarget: "commonjs2"
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader'
-        ],
+        use: ["vue-style-loader", "css-loader"]
       },
       {
         test: /\.scss$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'sass-loader'
-        ],
+        use: ["vue-style-loader", "css-loader", "sass-loader"]
       },
       {
         test: /\.sass$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'sass-loader?indentedSyntax'
-        ],
+        use: ["vue-style-loader", "css-loader", "sass-loader?indentedSyntax"]
       },
       {
         test: /\.vue$/,
-        loader: 'vue-loader',
+        loader: "vue-loader",
         options: {
           loaders: {
             // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
             // the "scss" and "sass" values for the lang attribute to the right configs here.
             // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': [
-              'vue-style-loader',
-              'css-loader',
-              'sass-loader'
-            ],
-            'sass': [
-              'vue-style-loader',
-              'css-loader',
-              'sass-loader?indentedSyntax'
+            scss: ["vue-style-loader", "css-loader", "sass-loader"],
+            sass: [
+              "vue-style-loader",
+              "css-loader",
+              "sass-loader?indentedSyntax"
             ]
           }
           // other vue-loader options go here
@@ -64,46 +49,55 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         // exclude: /node_modules/,
-        include: [
-          resolve('src'),
-          resolve('node_modules/element-ui'),
-        ]
+        include: [resolve("src"), resolve("node_modules/element-ui")]
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          name: '[name].[ext]?[hash]'
+          name: "[name].[ext]?[hash]"
         }
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 10000,
-          name: path.resolve(__dirname, './dist/satic/fonts/[name].[hash:7].[ext]')
+          name: path.resolve(
+            __dirname,
+            "./dist/satic/fonts/[name].[hash:7].[ext]"
+          )
         }
       }
     ]
   },
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
-      '@components': resolve('src/components'),
+      vue$: "vue/dist/vue.esm.js",
+      "@": resolve("src"),
+      "@components": resolve("src/components")
     },
-    extensions: ['*', '.js', '.vue', '.json']
+    extensions: ["*", ".js", ".vue", ".json"]
   },
-  externals : {
-    vue: {
-      root: 'Vue',
-      commonjs: 'vue',
-      commonjs2: 'vue',
-      amd: 'vue'
-    }
-  },
+  externals: [
+    {
+      vue: {
+        root: "Vue",
+        commonjs: "vue",
+        commonjs2: "vue",
+        amd: "vue"
+      }
+    },
+    "element-ui/lib/input",
+    "element-ui/lib/scrollbar",
+    "element-ui/lib/input",
+    "element-ui/lib/button",
+    "element-ui/lib/dropdown",
+    "element-ui/lib/dropdown-menu",
+    "element-ui/lib/dropdown-item",
+  ],
   devServer: {
     historyApiFallback: true,
     noInfo: true,
@@ -112,5 +106,5 @@ module.exports = {
   performance: {
     hints: false
   },
-  devtool: '#eval-source-map'
-}
+  devtool: "#eval-source-map"
+};
